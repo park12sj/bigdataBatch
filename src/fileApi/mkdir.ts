6cbc775a3fa54f29b6006d9d02d47c0368e624c8
psj <park12sj@gmail.com>
2021-03-28 18:51:11 +0900

    make fake data for csv format

--- src/fileApi/mkdir.ts
+++ src/fileApi/mkdir.ts
@@ -4,7 +4,7 @@ import {fileExist} from './fileExist'
 export const mkdir = (dirname: string): Promise<string> =>
     new Promise(async (resolve, reject) => {
         const alreadyExists = await fileExist(dirname)
-        alreadyExists ? resolve(dirname) : mkdirp(dirname) && resolve(dirname)
+        alreadyExists ? resolve(dirname) : await mkdirp(dirname) && resolve(dirname)
         // mkdirp(dirname, error => error ? reject(error) : resolve(dirname))
     }
 )
