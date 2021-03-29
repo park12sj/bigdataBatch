3b28a0ef4e4488848167bafc66e7f277afdd8c6c
psj <park12sj@gmail.com>
2021-03-29 18:24:33 +0900

    read fake csv data

--- src/fileApi/index.ts
+++ src/fileApi/index.ts
@@ -1,9 +1,10 @@
-import {fileExist} from '../fileApi/fileExist'
-import {mkdir} from '../fileApi/mkdir'
-import {rmdir} from '../fileApi/rmdir'
-import {writeFile} from '../fileApi/writeFile'
-import {readFile} from '../fileApi/readFile'
-import {appendFile} from '../fileApi/appendFile'
-import {deleteFile} from '../fileApi/deleteFile'
+import {fileExist} from './fileExist'
+import {mkdir} from './mkdir'
+import {rmdir} from './rmdir'
+import {writeFile} from './writeFile'
+import {readFile} from './readFile'
+import {appendFile} from './appendFile'
+import {deleteFile} from './deleteFile'
+import {readFileGenerator} from './readFileGenerator'
 
-export {fileExist, mkdir, rmdir, writeFile, readFile, appendFile, deleteFile}
+export {fileExist, mkdir, rmdir, writeFile, readFile, appendFile, deleteFile, readFileGenerator}
